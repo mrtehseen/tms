f2c83e48abeaf13d70d77efe4efa011dd02caf51
Shijith Mohanan <developer.shijith@gmail.com>
2017-12-30 23:34:03 +0530

    jwt token added

--- server/app.js
+++ server/app.js
@@ -1,26 +1,39 @@
 const express = require('express');
+const app = express();
 const mysql = require('mysql');
 const qs = require('querystring');
-const bodyParser = require('body-parser');   
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const config = require('./config'); // get our config file
+
+
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 3000; // used to create, sign, and verify tokens
+app.set('superSecret', config.secret); // secret variable
+
 
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database : 'tms'
+    database: 'tms'
 });
 
-db.connect((err)=>{
-    if(err){
+db.connect((err) => {
+    if (err) {
         throw err;
     }
     console.log('Mysql Connected');
 });
 
-const app = express();
 
-app.use(bodyParser.json({limit:'5mb'}));    
-app.use(bodyParser.urlencoded({extended:true, limit:'5mb'}));  
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Add headers
 app.use(function (req, res, next) {
@@ -42,29 +55,90 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.post('/authenticate',(req,res) => {
 
-    let sql = `select * from users where username='${req.body.username}' AND password = '${req.body.password}' `;
-    let query = db.query( sql ,(err,result) => {
-        if(err) throw err;
-        console.log(req.body.username);
-         // return the information including token as JSON
-         res.json({
-            success: true,
-            message: 'Enjoy your token!',
-            token: 'tmstoken'
-         });
+// =======================
+// routes ================
+// =======================
+// basic route
+app.get('/', function (req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
 
-         return res.send();
+// API ROUTES -------------------
 
-        //  return res.status(403).send({ 
-        //     success: false, 
-        //     message: 'No token provided.' 
-        // });
-    
+// get an instance of the router for api routes
+var apiRoutes = express.Router();
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function (req, res) {
+    res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function (req, res) {
+
+    let sql = `select * from users`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+
+apiRoutes.post('/authenticate', (req, res) => {
+
+    let sql = `select * from users where username='${req.body.username}' AND password = '${req.body.password}'`;
+    let query = db.query(sql, (err, user) => {
+
+        if (err) throw err;
+
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+
+            console.log(user);
+
+            // check if password matches
+            if (user[0].password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+
+              
+                // if user is found and password is right
+                // create a token with only our given payload
+                // we don't want to pass in the entire user since that has the password
+                const payload = {
+                username: user[0].username 
+                };
+                var token = jwt.sign(payload, app.get('superSecret'));
+  
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+
+                res.send();
+            }
+
+        }
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server started on 3000');
-});
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
+
+// API ROUTES -------------------
+// we'll get to these in a second
+
+// =======================
+// start the server ======
+// =======================
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  TMSAPI:'http://localhost:3000',
+  TMSAPI:'http://localhost:3000/api',
 };
