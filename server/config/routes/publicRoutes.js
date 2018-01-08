bfdc0691c53d157ea3b432295598fd754e3f23cb
Shijith Mohanan <developer.shijith@gmail.com>
2018-01-08 22:43:21 +0530

    Removed server folder

--- server/config/routes/publicRoutes.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'POST /user': 'UserController.register',
-  'POST /register': 'UserController.register', // alias for POST /user
-  'POST /login': 'UserController.login',
-  'POST /validate': 'UserController.validate',
-};
